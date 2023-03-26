bdf64b22bce0e26f3be6f8141f7da0cdb6737dc0
Jeffchua96 <jeffersonchua@pursuit.org>
2023-03-26 09:40:35 -0400

    fetch not done

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-<article class="card">
-  <h2>CATEGORY</h2>
-  <p>QUESTION</p>
-  <button>Show Answer</button>
-  <p class="hidden">CORRECT ANSWER</p>
-</article>
+
+
+const form  = document.querySelector("form")
+const BASE_URL = https://wttr.in/Montreal?format=j1
+form.addEventListener("submit", (event)=>{
+    event.preventDefault();
+
+    fetch (https://opentdb.com/api.php?amount=10 )
+    .then((response) => response.json())
+    .then((json) => {
+        json.results.forEach((result) =>{
+            functionName(result)
+        })
+    })
+    .catch(functionName);
+
+}
+ let showAnswer= document.querySelector("show-answer)"
+    Clipboard.addEventListener("click" , (event) =>{
+        event.preventDefault();
+    })
+ 
